2713d2438beb520b29e34aa7faa5af43dee30fd0
Christian Micklisch <christian.micklisch@successwithsos.com>
2018-09-14 21:00:20 -0400

    removed unecessary files

--- assets/js/bootstrap.js
+++ assets/js/bootstrap.js
@@ -57,6 +57,62 @@ Bootstrap = (function() {
                 onHover: function() {
                     document.getElementById('header').innerHTML = 'Dhanu Dnyaniyaachi';
                 }
+            },
+            { 
+                id: 2,
+                importance: Math.random(),
+                scale: Math.random(),
+                onHover: function() {
+                    document.getElementById('header').innerHTML = 'The Secret Life of Bees';
+                }
+            },
+            { 
+                id: 3,
+                importance: Math.random(),
+                scale: Math.random(),
+                onHover: function() {
+                    document.getElementById('header').innerHTML = 'Das Buch der Bilder';
+                }
+            },
+            { 
+                id: 4,
+                importance: Math.random(),
+                scale: Math.random(),
+                onHover: function() {
+                    document.getElementById('header').innerHTML = 'Das Knaben Wunderhorn';
+                }
+            },
+            { 
+                id: 5,
+                importance: Math.random(),
+                scale: Math.random(),
+                onHover: function() {
+                    document.getElementById('header').innerHTML = 'Das Stunden-Buch';
+                }
+            },
+            { 
+                id: 6,
+                importance: Math.random(),
+                scale: Math.random(),
+                onHover: function() {
+                    document.getElementById('header').innerHTML = 'Day by Day';
+                }
+            },
+            { 
+                id: 7,
+                importance: Math.random(),
+                scale: Math.random(),
+                onHover: function() {
+                    document.getElementById('header').innerHTML = 'Death and Fame: Poems 1993–1997';
+                }
+            },
+            { 
+                id: 8,
+                importance: Math.random(),
+                scale: Math.random(),
+                onHover: function() {
+                    document.getElementById('header').innerHTML = 'Dhanu Dnyaniyaachi';
+                }
             }
         ])
     }
