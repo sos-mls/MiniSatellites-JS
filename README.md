740bbb3346c3ba3fd7807d708e15378c0ec07c2e
Christian Micklisch <christian.micklisch@successwithsos.com>
2018-09-17 23:35:54 -0400

    Updated the readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # MiniSatellites-JS
+
+To run this program simply install [npm](https://www.npmjs.com/), install packages, build the JS project, and open up the `index.html` file in your browser. The build process uses webpack to generate the `build/index.js` file. The actual file used to generate the `build/index.js` file is stored in the `assets/index.js` directory.
+
+```shell
+$ git clone https://github.com/sos-mls/MiniSatellites-JS
+$ cd MiniSatellites-JS
+$ npm install
+$ npm run-script build
+$ open ./index.html
+```
--- assets/index.js
+++ assets/index.js
@@ -1,126 +1,122 @@
 import SolarSystem from 'solarsystem-graph'
 
-function init() {
-    SolarSystem(createContainer(), [
-        { 
-            id: 2,
-            importance: Math.random(),
-            scale: Math.random(),
-            onHover: function() {
-                document.getElementById('header').innerHTML = 'The Secret Life of Bees';
-            }
-        },
-        { 
-            id: 3,
-            importance: Math.random(),
-            scale: Math.random(),
-            onHover: function() {
-                document.getElementById('header').innerHTML = 'Das Buch der Bilder';
-            }
-        },
-        { 
-            id: 4,
-            importance: Math.random(),
-            scale: Math.random(),
-            onHover: function() {
-                document.getElementById('header').innerHTML = 'Das Knaben Wunderhorn';
-            }
-        },
-        { 
-            id: 5,
-            importance: Math.random(),
-            scale: Math.random(),
-            onHover: function() {
-                document.getElementById('header').innerHTML = 'Das Stunden-Buch';
-            }
-        },
-        { 
-            id: 6,
-            importance: Math.random(),
-            scale: Math.random(),
-            onHover: function() {
-                document.getElementById('header').innerHTML = 'Day by Day';
-            }
-        },
-        { 
-            id: 7,
-            importance: Math.random(),
-            scale: Math.random(),
-            onHover: function() {
-                document.getElementById('header').innerHTML = 'Death and Fame: Poems 1993–1997';
-            }
-        },
-        { 
-            id: 8,
-            importance: Math.random(),
-            scale: Math.random(),
-            onHover: function() {
-                document.getElementById('header').innerHTML = 'Dhanu Dnyaniyaachi';
-            }
-        },
-        { 
-            id: 2,
-            importance: Math.random(),
-            scale: Math.random(),
-            onHover: function() {
-                document.getElementById('header').innerHTML = 'The Secret Life of Bees';
-            }
-        },
-        { 
-            id: 3,
-            importance: Math.random(),
-            scale: Math.random(),
-            onHover: function() {
-                document.getElementById('header').innerHTML = 'Das Buch der Bilder';
-            }
-        },
-        { 
-            id: 4,
-            importance: Math.random(),
-            scale: Math.random(),
-            onHover: function() {
-                document.getElementById('header').innerHTML = 'Das Knaben Wunderhorn';
-            }
-        },
-        { 
-            id: 5,
-            importance: Math.random(),
-            scale: Math.random(),
-            onHover: function() {
-                document.getElementById('header').innerHTML = 'Das Stunden-Buch';
-            }
-        },
-        { 
-            id: 6,
-            importance: Math.random(),
-            scale: Math.random(),
-            onHover: function() {
-                document.getElementById('header').innerHTML = 'Day by Day';
-            }
-        },
-        { 
-            id: 7,
-            importance: Math.random(),
-            scale: Math.random(),
-            onHover: function() {
-                document.getElementById('header').innerHTML = 'Death and Fame: Poems 1993–1997';
-            }
-        },
-        { 
-            id: 8,
-            importance: Math.random(),
-            scale: Math.random(),
-            onHover: function() {
-                document.getElementById('header').innerHTML = 'Dhanu Dnyaniyaachi';
-            }
+SolarSystem(createContainer(), [
+    { 
+        id: 2,
+        importance: Math.random(),
+        scale: Math.random(),
+        onHover: function() {
+            document.getElementById('header').innerHTML = 'The Secret Life of Bees';
         }
-    ])
-}
+    },
+    { 
+        id: 3,
+        importance: Math.random(),
+        scale: Math.random(),
+        onHover: function() {
+            document.getElementById('header').innerHTML = 'Das Buch der Bilder';
+        }
+    },
+    { 
+        id: 4,
+        importance: Math.random(),
+        scale: Math.random(),
+        onHover: function() {
+            document.getElementById('header').innerHTML = 'Das Knaben Wunderhorn';
+        }
+    },
+    { 
+        id: 5,
+        importance: Math.random(),
+        scale: Math.random(),
+        onHover: function() {
+            document.getElementById('header').innerHTML = 'Das Stunden-Buch';
+        }
+    },
+    { 
+        id: 6,
+        importance: Math.random(),
+        scale: Math.random(),
+        onHover: function() {
+            document.getElementById('header').innerHTML = 'Day by Day';
+        }
+    },
+    { 
+        id: 7,
+        importance: Math.random(),
+        scale: Math.random(),
+        onHover: function() {
+            document.getElementById('header').innerHTML = 'Death and Fame: Poems 1993–1997';
+        }
+    },
+    { 
+        id: 8,
+        importance: Math.random(),
+        scale: Math.random(),
+        onHover: function() {
+            document.getElementById('header').innerHTML = 'Dhanu Dnyaniyaachi';
+        }
+    },
+    { 
+        id: 2,
+        importance: Math.random(),
+        scale: Math.random(),
+        onHover: function() {
+            document.getElementById('header').innerHTML = 'The Secret Life of Bees';
+        }
+    },
+    { 
+        id: 3,
+        importance: Math.random(),
+        scale: Math.random(),
+        onHover: function() {
+            document.getElementById('header').innerHTML = 'Das Buch der Bilder';
+        }
+    },
+    { 
+        id: 4,
+        importance: Math.random(),
+        scale: Math.random(),
+        onHover: function() {
+            document.getElementById('header').innerHTML = 'Das Knaben Wunderhorn';
+        }
+    },
+    { 
+        id: 5,
+        importance: Math.random(),
+        scale: Math.random(),
+        onHover: function() {
+            document.getElementById('header').innerHTML = 'Das Stunden-Buch';
+        }
+    },
+    { 
+        id: 6,
+        importance: Math.random(),
+        scale: Math.random(),
+        onHover: function() {
+            document.getElementById('header').innerHTML = 'Day by Day';
+        }
+    },
+    { 
+        id: 7,
+        importance: Math.random(),
+        scale: Math.random(),
+        onHover: function() {
+            document.getElementById('header').innerHTML = 'Death and Fame: Poems 1993–1997';
+        }
+    },
+    { 
+        id: 8,
+        importance: Math.random(),
+        scale: Math.random(),
+        onHover: function() {
+            document.getElementById('header').innerHTML = 'Dhanu Dnyaniyaachi';
+        }
+    }
+])
 
 function createContainer() {
     var container = document.createElement( 'div' );
     document.body.appendChild( container );
     return container;
 }
-
-init();
